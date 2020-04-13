cdcf274af368dc8b8d039f4728d3ed70600031f2
zriemann <tfosorcimz@hotmail.com>
2020-04-13 23:54:55 +0800

    [update] build-essential.sh

--- install/build-essential.sh
+++ install/build-essential.sh
@@ -11,8 +11,21 @@ enable_dbg=1
 # 安装基础开发环境
 echo_msg "Intall build-essential(安装基础开发环境)"
 if [ "os_name" = "Ubuntu" ]; then
-    sudo ${PM} install build-essential -y
+    echo_msg "https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/"
+    echo_msg "请先设置清华镜像源, 完成后回车确认"
+    read pick
+    sudo apt update
+    sudo apt-get update
+    sudo apt -y install build-essential
+    sudo apt -y install libncurses5-dev 
+    sudo apt -y install libpng-dev
+    sudo apt -y install libxpm-dev 
+    sudo apt -y install libtiff-dev
+    sudo apt -y install libjpeg-dev 
+    sudo apt -y install libgif-dev 
+    sudo apt -y install libgtk-3-dev
 else
+    sudo yum update
     sudo yum -y groupinstall "Development Tools"
     cd /etc
     if [ "$LD_LIBRARY_PATH" = "" ]; then
@@ -33,13 +46,16 @@ sudo ${PM} install cmake -y
 echo_msg "Install ncurses-dev(for emacs)"
 if [ "os_name" = "Ubuntu" ]; then
 	sudo apt-get -y install libncurses5-dev
-    sudo apt-get -y install pkg-config
+    	sudo apt-get -y install pkg-config
+	# Ubuntu20.04
+	sudo apt -y install ncurses-dev
 else
 	sudo yum -y install ncurses-devel
 fi
 
 sudo ${PM} install g++-multilib -y
 
+global_name=global-6.6.4
 echo_inf "Installing ggtags? (yes|no)"
 read pick
 if [ "$pick" = "yes" ]; then
@@ -47,36 +63,59 @@ if [ "$pick" = "yes" ]; then
     rm -fr global.src
     mkdir global.src
     cd global.src
-    wget https://ftp.gnu.org/pub/gnu/global/global-6.6.2.tar.gz
-    tar zxf global-6.6.2.tar.gz
-    cd global-6.6.2
-    ./configure --with-exuberant-ctags=/usr/local/bin/ctags
-    make -j8
-    sudo make install
-fi
-
-echo_inf "Installing gdb? (yes|no)"
-read pick
-if [ "$pick" = "yes" ]; then
-    cd /tmp
-    rm -fr gdb.src
-    mkdir gdb.src
-    cd gdb.src
-    wget https://ftp.gnu.org/gnu/gdb/gdb-8.1.tar.gz
-    tar zxf gdb-8.1.tar.gz
-    cd gdb-8.1
+    #wget https://ftp.gnu.org/pub/gnu/global/global-6.6.2.tar.gz
+    wget https://mirrors.tuna.tsinghua.edu.cn/gnu/global/${global_name}.tar.gz
+    tar -zxf ${global_name}.tar.gz
+    cd ${global_name}
+    #./configure --with-exuberant-ctags=/usr/local/bin/ctags
     ./configure
     make -j8
     sudo make install
-    gdb --version
 fi
 
-echo_inf "install flex/bison(词法/语法分析器/32bit) (yes|no)"
-read pick
-if [ "yes" = "$pick" ]; then
-    sudo ${PM} install flex -y
-    sudo ${PM} install bison -y
-fi
+#=============================================================================
+# 安装emacs25.3
+cd /tmp
+emacs_name=emacs-25.3
+echo_msg "Install ${emacs_name}"
+
+echo_msg "download ${emacs_name} to /tmp"
+# wget https://ftp.gnu.org/gnu/emacs/${emacs_name}.tar.gz
+wget https://mirrors.tuna.tsinghua.edu.cn/gnu/emacs/${emacs_name}.tar.gz
+
+echo_msg "Build and install ${emacs_name}"
+tar zxf ${emacs_name}.tar.gz
+cd ${emacs_name}
+./autogen.sh
+./configure
+make -j8
+sudo make install
+
+echo_msg "download emacs.d"
+cd /tmp
+
+#echo_inf "Installing gdb? (yes|no)"
+#read pick
+#if [ "$pick" = "yes" ]; then
+#    cd /tmp
+#    rm -fr gdb.src
+#    mkdir gdb.src
+#    cd gdb.src
+#    wget https://ftp.gnu.org/gnu/gdb/gdb-8.1.tar.gz
+#    tar zxf gdb-8.1.tar.gz
+#    cd gdb-8.1
+#    ./configure
+#    make -j8
+#    sudo make install
+#    gdb --version
+#fi
+
+#echo_inf "install flex/bison(词法/语法分析器/32bit) (yes|no)"
+#read pick
+#if [ "yes" = "$pick" ]; then
+#    sudo ${PM} install flex -y
+#    sudo ${PM} install bison -y
+#fi
 
 cd $cmd_dir
 exit 0
--- install/emacs.sh
+++ install/emacs.sh
@@ -18,49 +18,41 @@ rm -fr ${emacs_name}*
 if [ "$os_name" = "CentOS" ]; then
     #安装必要的库
     sudo yum install gcc make ncurses-devel giflib-devel libjpeg-devel libtiff-devel
+else
+	sudo apt -y install ncurses-dev libtiff-dev libjpeg-dev giflib-dev libgtk-3-dev
 fi
 
 echo_msg "download ${emacs_name} to /tmp"
-wget https://ftp.gnu.org/gnu/emacs/${emacs_name}.tar.gz
+# wget https://ftp.gnu.org/gnu/emacs/${emacs_name}.tar.gz
+wget https://mirrors.tuna.tsinghua.edu.cn/gnu/emacs/${emacs_name}.tar.gz
 
 echo_msg "Build and install ${emacs_name}"
 tar zxf ${emacs_name}.tar.gz
 cd ${emacs_name}
 ./autogen.sh
-./configure --without-x
+./configure
 make -j4
 sudo make install
 cd ..
 
 echo_msg "download emacs.d"
 cd /tmp
-rm -f master.zip
-wget https://github.com/redguardtoo/emacs.d/archive/master.zip
-unzip master.zip
-mv emacs.d-master/ ~/.emacs.d
+# rm -f master.zip
+# wget https://github.com/redguardtoo/emacs.d/archive/master.zip
+# unzip master.zip
+# mv emacs.d-master/ ~/.emacs.d
 
+#cat <<!GTAGS! >> ~/.emacs.d/init.el
 
-echo_msg "install ggtags (global)"
-cd /tmp
-gtag_version=global-6.6.2
-wget http://tamacom.com/global/${gtag_version}.tar.gz
-cd ${gtag_version}
-
-./configure --with-exuberant-ctags=/usr/local/bin/ctags
-make
-sudo make isntall
-
-cat <<!GTAGS! >> ~/.emacs.d/init.el
-
-;; add ggtags package
-(require-package 'ggtags)
-(require 'ggtags)
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-              (ggtags-mode 1))))
-(provide 'init-ggtags)
-!GTAGS!
+#;; add ggtags package
+#(require-package 'ggtags)
+#(require 'ggtags)
+#(add-hook 'c-mode-common-hook
+#          (lambda ()
+#            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+#              (ggtags-mode 1))))
+#(provide 'init-ggtags)
+#!GTAGS!
 
 echo_msg "Install ${emacs_name} down"
 echo_inf "Try emacs now!"
