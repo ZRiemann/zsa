dd8d2c7807a1ae936cfb800ad00ed79b5136807a
Z.Riemann <zwp_showsun@163.com>
2021-06-10 01:03:52 -0700

    Update build essential

--- install/build-essential.sh
+++ install/build-essential.sh
@@ -14,16 +14,39 @@ if [ "$os_name" = "Ubuntu" ]; then
     echo_msg "https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/"
     echo_msg "请先设置清华镜像源, 完成后回车确认"
     read pick
+    # 最新git
+    sudo add-apt-repository ppa:git-core/ppa
+    # gcc-7 gcc-8 gcc-9
+    sudo apt install software-properties-common
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    # emacs27
+    sudo add-apt-repository ppa:kelleyk/emacs
+
+    # update all source
     sudo apt update
     sudo apt-get update
     sudo apt -y install build-essential
-    sudo apt -y install libncurses5-dev 
+    sudo apt -y install libncurses5-dev
     sudo apt -y install libpng-dev
-    sudo apt -y install libxpm-dev 
+    sudo apt -y install libxpm-dev
     sudo apt -y install libtiff-dev
-    sudo apt -y install libjpeg-dev 
-    sudo apt -y install libgif-dev 
+    sudo apt -y install libjpeg-dev
+    sudo apt -y install libgif-dev
     sudo apt -y install libgtk-3-dev
+    echo_msg "Install latest stable Git"
+    sudo apt install git
+    echo_msg "Install emacs27"
+    sudo apt install emacs27
+    echo_msg "Install gcc/++-7,8,9"
+    ehco_msg "switch version: sudo update-alternatives --config gcc/g++"
+    sudo apt -y install gcc-7 g++-7 gcc-8 g++-8 gcc-9 g++-9
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 90
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 80
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 70
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70
+
 else
     sudo yum update
     sudo yum -y groupinstall "Development Tools"
@@ -46,15 +69,15 @@ sudo ${PM} install cmake -y
 echo_msg "Install ncurses-dev(for emacs)"
 if [ "$os_name" = "Ubuntu" ]; then
 	sudo apt-get -y install libncurses5-dev
-    	sudo apt-get -y install pkg-config
+    sudo apt-get -y install pkg-config
 	# Ubuntu20.04
 	sudo apt -y install ncurses-dev
 else
 	sudo yum -y install ncurses-devel
 fi
 
-sudo ${PM} install g++-multilib -y
-sudo ${PM} install global gdb -y
+#sudo ${PM} install g++-multilib -y
+sudo ${PM} install gdb -y
 
 #global_name=global-6.6.4
 #echo_inf "Installing ggtags? (yes|no)"
@@ -95,21 +118,39 @@ sudo ${PM} install global gdb -y
 #echo_msg "download emacs.d"
 #cd /tmp
 
-#echo_inf "Installing gdb? (yes|no)"
+#gdb_version=10.2
+#echo_inf "Installing gdb${gdb_version}? (yes|no)"
 #read pick
 #if [ "$pick" = "yes" ]; then
 #    cd /tmp
 #    rm -fr gdb.src
 #    mkdir gdb.src
 #    cd gdb.src
-#    wget https://ftp.gnu.org/gnu/gdb/gdb-8.1.tar.gz
-#    tar zxf gdb-8.1.tar.gz
-#    cd gdb-8.1
+#    wget https://ftp.gnu.org/gnu/gdb/gdb-${gdb_version}.tar.gz
+#    tar zxf gdb-${gdb_version}.tar.gz
+#    cd gdb-${gdb_version}
 #    ./configure
 #    make -j8
 #    sudo make install
 #    gdb --version
 #fi
+
+global_version=6.6
+echo_inf "Installing global${global_version}? (yes|no)"
+read pick
+if [ "$pick" = "yes" ]; then
+    cd /tmp
+    rm -fr global.src
+    mkdir global.src
+    cd global.src
+    wget https://ftp.gnu.org/gnu/global/global-${global_version}.tar.gz
+    tar zxf global-${global_version}.tar.gz
+    cd global-${global_version}
+    ./configure
+    make -j8
+    sudo make install
+    global --version
+fi
 
 #echo_inf "install flex/bison(词法/语法分析器/32bit) (yes|no)"
 #read pick
