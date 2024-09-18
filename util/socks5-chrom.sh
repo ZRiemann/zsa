e4b4ef565f0b6cee467514a194f6254347f6f80a
Z.Riemann <25452483@qq.com>
2024-09-18 12:00:09 +0800

    Add socks5-chrom windows commend

--- util/socks5-chrom.sh
+++ util/socks5-chrom.sh
@@ -1,1 +1,2 @@
 /usr/bin/google-chrome --user-data-dir="$HOME/proxy-profile" --proxy-server="socks5://localhost:${SSH_SOCKS5_PORT}"
+# sudo shortcut\GptBrowser.exe --user-data-dir="$HOME\proxy-file" --proxy-server="socks5://172.17.211.193:9090" 
